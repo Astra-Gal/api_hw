506c0171427c0c5e15f647e597e8a0b874e7e23f
Astra-Gal <giulia.galastro@gmail.com>
2021-01-18 09:03:05 +0000

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" v-if="dollsHouses">
     <h1>Adventures in the Rijksmuseum</h1>
-    <dolls-houses></dolls-houses>
+    <h2>Choose your adventure...</h2>
+    <dolls-houses :dollshouses="dollsHouses"></dolls-houses>
 
   </div>
 </template>
 
 <script>
-import config from '../config.js'
 import DollsHouses from './components/DollsHouses.vue'
 
 export default {
   name: 'App',
   data() {
     return {
-      things: [],
+      dollsHouses: null,
       selectedThing: null
     }
   },
@@ -22,8 +22,12 @@ export default {
     'dolls-houses': DollsHouses
   },
   mounted() {
-    fetch(`https://www.rijksmuseum.nl/api/en/collection?key=Yye8TNJH&q=Doll+house&imgonly=True`)
-    .then(response => console.log(response.json()))
+    fetch(`https://www.rijksmuseum.nl/api/en/collection?key=TYyQYD1w=Doll+house&imgonly=True&toppieces=True`)
+    .then(response => response.json())
+    .then(dollsHouses => this.dollsHouses = dollsHouses.artObjects)
+    // .then((response) => {
+    //   let artObjects = map(response.artObjects,)
+    //   console.log(response.json())})
 
   }
 }
@@ -39,4 +43,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h2, h3 {
+    font-family: 'Antic Didone', serif;
+}
+
 </style>
--- src/components/DollsHouses.vue
+++ src/components/DollsHouses.vue
@@ -1,18 +1,57 @@
 <template lang="html">
-    <span>
-        <h2>Doll's Houses</h2>
+    <div>
+        <span v-if="dollshouses">
+        <h3>Doll's Houses</h3>
 
     </span>
+    <div>
+        <button v-on:click="testFilter"> Test filter </button>
+    </div>
+        <div>
+        <button v-on:click="testTruthy"> Test Truthy </button>
+    </div>
+    <div>
+        <button v-on:click="testDollHouseTitles"> Somethinuthy </button>
+    </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'dolls-houses'
+    name: 'dolls-houses',
+    props: ['dollshouses'],
+    data() {
+        return {
+            something: null
+        }
+    },
+    computed: {
+        getDollHouseTitles: function() {
+            return this.dollshouses.filter( dollshouse => dollshouse.title.match(/Dolls/gi) != null)
+            }
+    },
+    methods: {
+        testFilter: function(){
+            
+            console.log(this.dollshouses[0].title.match(/Dolls/gi) !== null); // true
+            console.log(this.dollshouses[4].title.match(/Dolls/gi) !== null); // false
+        },
+        testTruthy: function() {
+            console.log(this.dollshouses.filter((dollshouse) => {
+                typeof dollshouse.title.match(/Doll/gi) !== -1; 
+            }))
+        },
+        testDollHouseTitles : function() {
+            this.something = this.dollshouses.filter((dollshouse) => {
+                dollshouse.title.match(/Dolls/gi) !== null;
+            })
+            }
+    }
 }
 </script>
 
 <style lang="css" scoped>
-h2 {
+h2, h3 {
     font-family: 'Antic Didone', serif;
 }
 
